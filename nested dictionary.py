a8c0669212a84e3cb3196f6eb886bfbd86ed405a
AGR1234567 <ridgeutube@gmail.com>
2024-10-30 08:28:30 +0000

    Add files via upload

---
+++ nested dictionary.py
@@ -0,0 +1,11 @@
+classroom = {'Student1':{'Age':11,
+                            'Marks':[96, 97, 89, 100, 98]
+                            },
+             'Student2':{'Age':12,
+                             'Marks2':[99, 95, 100, 100, 88]
+                             }
+                             }
+
+print(classroom.values())
+print(classroom.keys())
+print(classroom['Student2']['Age'])
